ed73ce36656c7ea82006577f26d59f04461c9239
Kit4y <381382770@qq.com>
2019-02-15 13:48:18 +0800

    Add template

--- blog/views.py
+++ blog/views.py
@@ -23,12 +23,12 @@ class BlogTypeViewSet(viewsets.ModelViewSet):
 # 使用django自带模板
 def blog_list(request):
     context = {'blogs': Blog.objects.all()}
-    return render_to_response('blog_list.html', context)
+    return render_to_response('blog/blog_list.html', context)
 
 
 def blog_detail(request, blog_pk):
     context = {'blog': get_object_or_404(Blog, pk=blog_pk)}
-    return render_to_response('blog_detail.html', context)
+    return render_to_response('blog/blog_detail.html', context)
 
 
 def blogs_with_type(request, blog_type_pk):
@@ -36,4 +36,4 @@ def blogs_with_type(request, blog_type_pk):
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
     context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type'] = blog_type
-    return render_to_response('blogs_with_type.html', context)
+    return render_to_response('blog/blogs_with_type.html', context)
