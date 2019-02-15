487ecb1f83572bf8a09c2e352a29829b96d01e7f
Kit4y <381382770@qq.com>
2019-02-15 16:16:17 +0800

    repair bugs

--- blog/views.py
+++ blog/views.py
@@ -22,7 +22,7 @@ class BlogTypeViewSet(viewsets.ModelViewSet):
 
 # 使用django自带模板
 def blog_list(request):
-    context = {'blogs': Blog.objects.all()}
+    context = {'blogs': Blog.objects.all(), 'blog_types': BlogType.objects.all()}
     return render_to_response('blog/blog_list.html', context)
 
 
@@ -36,4 +36,5 @@ def blogs_with_type(request, blog_type_pk):
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
     context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type'] = blog_type
+    context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blogs_with_type.html', context)
