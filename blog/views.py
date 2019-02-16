efcdd3e79ef99f1dbc75426e422136804d0a45af
Kit4y <381382770@qq.com>
2019-02-16 11:25:15 +0800

    Add Paginator

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, render_to_response, get_object_or_404
 from rest_framework import viewsets
 from .models import Blog, BlogType
 from .serializers import BlogSerializers, BlogTypeSerializers
+from django.core.paginator import Paginator
 
 
 # 提供API
@@ -18,7 +19,23 @@ class BlogTypeViewSet(viewsets.ModelViewSet):
 
 # 使用django自带模板
 def blog_list(request):
-    context = {'blogs': Blog.objects.all(), 'blog_types': BlogType.objects.all()}
+    page_num = request.GET.get('page', 1)  # 获取页码参数
+    blogs_all_list = Blog.objects.all()
+    paginator = Paginator(blogs_all_list, 10)  # 每10页分页
+    page_of_blogs = paginator.get_page(page_num)  # 一旦出错就为1
+    currentr_page_num=page_of_blogs.number #当前页
+    page_range=list(range(max(currentr_page_num-2,1),currentr_page_num)) + list(range(currentr_page_num,min(currentr_page_num+2,paginator.num_pages)+1))
+    # 加上省略页码标记
+    if page_range[0] - 1 >= 2:
+        page_range.insert(0, '...')
+    if paginator.num_pages - page_range[-1] >= 2:
+        page_range.append('...')
+    # 加上首页和尾页
+    if page_range[0] != 1:
+        page_range.insert(0, 1)
+    if page_range[-1] != paginator.num_pages:
+        page_range.append(paginator.num_pages)
+    context = {'page_of_blogs': page_of_blogs, 'blog_types': BlogType.objects.all(),'page_range':page_range} # page_of_blogs带内容和页数
     return render_to_response('blog/blog_list.html', context)
 
 
