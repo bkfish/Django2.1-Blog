53e6476e5eadce12dd343f9f3346ede30715da2a
Kit4y <381382770@qq.com>
2019-02-21 13:54:14 +0800

    add api

--- blog/views.py
+++ blog/views.py
@@ -12,9 +12,9 @@ from read_statistics.utils import read_statistics_once_read
 
 def get_blog_list_common_data(request, blogs_all_list):
     paginator = Paginator(blogs_all_list, settings.EACH_PAGE_BLOGS_NUMBER)
-    page_num = request.GET.get('page', 1) # 获取url的页面参数（GET请求）
+    page_num = request.GET.get('page', 1)  # 获取url的页面参数（GET请求）
     page_of_blogs = paginator.get_page(page_num)
-    currentr_page_num = page_of_blogs.number # 获取当前页码
+    currentr_page_num = page_of_blogs.number  # 获取当前页码
     # 获取当前页码前后各2页的页码范围
     page_range = list(range(max(currentr_page_num - 2, 1), currentr_page_num)) + \
                  list(range(currentr_page_num, min(currentr_page_num + 2, paginator.num_pages) + 1))
@@ -33,7 +33,7 @@ def get_blog_list_common_data(request, blogs_all_list):
     blog_dates = Blog.objects.dates('created_time', 'month', order="DESC")
     blog_dates_dict = {}
     for blog_date in blog_dates:
-        blog_count = Blog.objects.filter(created_time__year=blog_date.year, 
+        blog_count = Blog.objects.filter(created_time__year=blog_date.year,
                                          created_time__month=blog_date.month).count()
         blog_dates_dict[blog_date] = blog_count
 
@@ -45,10 +45,12 @@ def get_blog_list_common_data(request, blogs_all_list):
     context['blog_dates'] = blog_dates_dict
     return context
 
+
 def blog_list(request):
     blogs_all_list = Blog.objects.all()
     context = get_blog_list_common_data(request, blogs_all_list)
     return render(request, 'blog/blog_list.html', context)
+
 
 def blogs_with_type(request, blog_type_pk):
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
@@ -57,22 +59,22 @@ def blogs_with_type(request, blog_type_pk):
     context['blog_type'] = blog_type
     return render(request, 'blog/blogs_with_type.html', context)
 
+
 def blogs_with_date(request, year, month):
     blogs_all_list = Blog.objects.filter(created_time__year=year, created_time__month=month)
     context = get_blog_list_common_data(request, blogs_all_list)
     context['blogs_with_date'] = '%s年%s月' % (year, month)
     return render(request, 'blog/blogs_with_date.html', context)
 
+
 def blog_detail(request, blog_pk):
     blog = get_object_or_404(Blog, pk=blog_pk)
     read_cookie_key = read_statistics_once_read(request, blog)
 
-    context = {}
-    context['previous_blog'] = Blog.objects.filter(created_time__gt=blog.created_time).last()
-    context['next_blog'] = Blog.objects.filter(created_time__lt=blog.created_time).first()
-    context['blog'] = blog
-    response = render(request, 'blog/blog_detail.html', context) # 响应
-    response.set_cookie(read_cookie_key, 'true') # 阅读cookie标记
+    context = {'previous_blog': Blog.objects.filter(created_time__gt=blog.created_time).last(),
+               'next_blog': Blog.objects.filter(created_time__lt=blog.created_time).first(), 'blog': blog}
+    response = render(request, 'blog/blog_detail.html', context)  # 响应
+    response.set_cookie(read_cookie_key, 'true')  # 阅读cookie标记
     return response
 
 
